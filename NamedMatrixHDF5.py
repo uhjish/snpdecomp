d9266cd2fd8d532b51e083c0a6dd795f1f90dd12
ajish <ajish@kilauea.mssm.edu>
2010-04-08 09:53:47 -0400

    sdjd

--- NamedMatrixHDF5.py
+++ NamedMatrixHDF5.py
@@ -7,24 +7,26 @@ class NamedMatrixHDF5(nm.NamedMatrix):
     def __init__(self,hdf5file,url):
         self.file=hdf5file
         self.url=url
-        self.h5 = h5py.File(self.file,'r')
-        self.getinitvars()    
+        self.unlock()
     def getinitvars(self):
-        hndl = self.h5[self.url]
-        self.colnames = hndl['colnames']
-        self.rownames = hndl['rownames']
-        self.matrix = hndl['matrix'] 
-    def __del__(self):
+        self.colnames = self.hndl['colnames']
+        self.rownames = self.hndl['rownames']
+        self.matrix = self.hndl['matrix'] 
+    def lock(self):
         self.h5.close()
-            
-
+    def unlock(self):
+        self.h5 = h5py.File(self.file)
+        self.hndl = self.h5[self.url]
+        self.getinitvars()    
+        
 
 def readfromfile(hdf5file, url, *args, **kwargs):
     mat = nm.readfromfile(*args, **kwargs)
     nmat = writeNamedMatrix( mat, hdf5file, url, overwrite=True)
     return nmat
 
-def writeNamedMatrix( matrix, hdf5file, groupurl, overwrite=False):
+def writeNamedMatrix( matrix, hdf5file, groupurl, overwrite=True):
+    print "groupurl: " + groupurl
     f = h5py.File(hdf5file, 'w')
     try:
         grp = f[groupurl]
